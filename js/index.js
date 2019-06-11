c17cefa39c0e68446de18624cc4753a1735156f3
sarawall1187 <sarawall1187@gmail.com>
2019-06-11 12:50:51 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+element.style.textAlign = 'center';
+
+let ul = document.createElement('ul');
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+element.appendChild(ul);
+
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
